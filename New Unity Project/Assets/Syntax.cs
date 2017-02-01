183d3a0dd2cd7419a6a2a896de3f367139e36db8
Unknown <kinzie.colleen@gmail.com>
2017-02-01 10:46:39 -0700

    Created syntax file

---
+++ New Unity Project/Assets/Syntax.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour {
+
+    public string myString = "10"; //identifier-- myString in this case-- has to be lowercase and one word, also cannot start with numeric value
+    public string myOtherString = "1";
+    public int myIn = 1;
+    public float myFloat = 1.2f;
+    public bool myBool = false;
+
+	// Use this for initialization
+	void Start () {
+        print("Hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
