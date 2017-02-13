398872ae3f562215cc9c6897a65ac07459560e13
Kinzie <kinzie.colleen@gmail.com>
2017-02-12 22:33:01 -0700

    updated ifstatements

--- New Unity Project/Assets/IfStatements.cs
+++ New Unity Project/Assets/IfStatements.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class IfStatements : MonoBehaviour {
 
 
     public bool myBool;
-    public string myString = "Dog";
+    public string myString = "Cat";
     public int num1;
     public int num2;
+    public int num3;
+    public int num4;
+    public string myString = "Roses"
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +19,7 @@ public class IfStatements : MonoBehaviour {
             print(true);
         }
 
-        if(myString == "Cat")
+        if(myString == "Cat") //== is a comparison tool. It's essential in conditionals.
         {
             print("Cat");
         }
@@ -26,7 +29,7 @@ public class IfStatements : MonoBehaviour {
             print(false);
         }
 
-        if(myString != "Cat")
+        if(myString != "Cat") //!= means "does not equal"
         {
             print(myString);
         }
@@ -38,7 +41,27 @@ public class IfStatements : MonoBehaviour {
 
         if (num1 + num2 != 7)
         {
-            print(num1 + num2);
+            print(num1 + num2); // meaning, print the true solution based on the two inputs
+        }
+
+        if  (num2/num4 == 6)
+        {
+            print(6);
+        }
+
+        if (num4/num3 == 12)
+        {
+            print(12);
+        }
+
+        if (num4 + num3 != 25)
+        {
+            print(num4 + num3);
+        }
+
+        if(myString == "tulips")
+        {
+            print("not roses") //print statements can be essentially whatever you want them to be
         }
 
 	}
